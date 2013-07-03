4ee008d22509545f889cde531cf0c2e8915b06be
David Zabner <david.zabner@gmail.com>
2013-07-03 16:01:52 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Texter::Application.config.secret_token = 'fe638a7efd08288252d74474f1eca696ee4e0bc9c23c269a13a262a59fc24cee3ecdc0fb31bf6bc4353bbf210bcf2e035e3da33c2e9edbcb94edaa7d7aa72091'
